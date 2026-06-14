2be66df54639a8cf742d47af54e75a70d01b8aa2
shahin-work <shahincanary@gmail.com>
2026-06-15 00:28:21 +0530

    Update attendance page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.57n76nbu9s"
+    "revision": "0.6nq4r9ror68"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
